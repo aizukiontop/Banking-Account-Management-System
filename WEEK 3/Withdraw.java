05e9c2bf0dbb8281adb17f5ffa5d2a7fde4f2b53
aizukiontop <mariondingal50@gmail.com>
2024-10-12 17:32:40 +0800

    Add files via upload

---
+++ WEEK 3/Withdraw.java
@@ -0,0 +1,11 @@
+// Withdraw class
+class Withdraw extends Transaction {
+    public Withdraw(String transactionID, double amount) {
+        super(transactionID, amount);
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Executing withdrawal of " + amount + " at " + timestamp);
+    }
+}
